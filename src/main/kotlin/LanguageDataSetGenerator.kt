902acd5013c77e26c4f9328232daa8037f0d3c33
Piotr Rusin <contact@reusingthewheel.net>
2020-06-14 01:29:58 +0200

    Adjust the code to another change in language-data

--- src/main/kotlin/LanguageDataSetGenerator.kt
+++ src/main/kotlin/LanguageDataSetGenerator.kt
@@ -1,8 +1,8 @@
 
 package com.github.piotr_rusin.language_data_set_generator
 
-import com.github.piotr_rusin.language_data.code.Code
-import com.github.piotr_rusin.language_data.value.Value
+import com.github.piotr_rusin.language_data.row.Code
+import com.github.piotr_rusin.language_data.row.Value
 import kotlin.random.Random
 
 fun getFeatureValueLanguageSetMap(values: List<Value>, codes:  Map<String, Code>): Map<Code, Set<String>> {
--- src/test/kotlin/LanguageDataSetGenerator.kt
+++ src/test/kotlin/LanguageDataSetGenerator.kt
@@ -1,7 +1,7 @@
 package com.github.piotr_rusin.language_data_set_generator
 
-import com.github.piotr_rusin.language_data.code.Code
-import com.github.piotr_rusin.language_data.value.Value
+import com.github.piotr_rusin.language_data.row.Code
+import com.github.piotr_rusin.language_data.row.Value
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
